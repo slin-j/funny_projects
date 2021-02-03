70d2907527ab9b6e857683363beae717027617ee
Nisls Jäggi <n-jaeggi@gmx.ch>
2021-02-03 13:14:31 +0100

    add sharp for gibb

---
+++ csharpgibb/weather_data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weather_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
