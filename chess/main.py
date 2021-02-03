ab9d0e52acfe676453658110410cc2825c8d465b
Nisls Jäggi <n-jaeggi@gmx.ch>
2021-02-03 14:29:09 +0100

    import all pictures and resize them

--- chess/main.py
+++ chess/main.py
@@ -5,11 +5,11 @@ import time
 # === CLASSES ===
 from board import board as chessBoard
 #===========================DEFINES==========================================
-SCR_WIDTH = 900
-SCR_HEIGHT = 950
+SCR_WIDTH = 750
+SCR_HEIGHT = 750
 START_X = 10
 START_Y = 75
-FIELD_DIMENSION = 100
+FIELD_DIMENSION = 75
 
 pygame.init()
 
@@ -25,20 +25,21 @@ chessBoard.drawGrid(background, START_X, START_Y, FIELD_DIMENSION)
 
 os.chdir(os.path.abspath('chess'))
 os.chdir(os.path.abspath('figure_pics'))
-img_bb = pygame.image.load('bb.png').convert_alpha()
-img_bk = pygame.image.load('bk.png').convert_alpha()
-img_bn = pygame.image.load('bn.png').convert_alpha()
-img_bp = pygame.image.load('bp.png').convert_alpha()
-img_bq = pygame.image.load('bq.png').convert_alpha()
-img_br = pygame.image.load('br.png').convert_alpha()
-img_wb = pygame.image.load('wb.png').convert_alpha()
-img_wk = pygame.image.load('wk.png').convert_alpha()
-img_wn = pygame.image.load('wn.png').convert_alpha()
-img_wp = pygame.image.load('wp.png').convert_alpha()
-img_wq = pygame.image.load('wq.png').convert_alpha()
-img_wr = pygame.image.load('wr.png').convert_alpha()
+img_bb = pygame.transform.scale(pygame.image.load('bb.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_bk = pygame.transform.scale(pygame.image.load('bk.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_bn = pygame.transform.scale(pygame.image.load('bn.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_bp = pygame.transform.scale(pygame.image.load('bp.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_bq = pygame.transform.scale(pygame.image.load('bq.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_br = pygame.transform.scale(pygame.image.load('br.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_wb = pygame.transform.scale(pygame.image.load('wb.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_wk = pygame.transform.scale(pygame.image.load('wk.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_wn = pygame.transform.scale(pygame.image.load('wn.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_wp = pygame.transform.scale(pygame.image.load('wp.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_wq = pygame.transform.scale(pygame.image.load('wq.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
+img_wr = pygame.transform.scale(pygame.image.load('wr.png').convert_alpha(), (FIELD_DIMENSION,FIELD_DIMENSION))
 
-img_bk = pygame.transform.scale(img_bk, (FIELD_DIMENSION,FIELD_DIMENSION))
+
+
 
 myfont = pygame.font.SysFont("monospace",16)
 
@@ -57,4 +58,6 @@ while True:
 
     window_surface.blit(background, (0, 0))
     window_surface.blit(img_bk, [START_X, START_Y])
+    window_surface.blit(img_wk, [START_X+75, START_Y])
+
     pygame.display.update()
