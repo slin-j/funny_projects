2d80b9a141b64c6afaf7bf61700709c391cc2f65
Nils Jäggi <n-jaeggi@gmx.ch>
2021-04-30 17:19:04 +0200

    c++

--- cpp/HelloWorld/helloWorld.cpp
+++ cpp/HelloWorld/helloWorld.cpp
@@ -1,6 +1,41 @@
 #include <iostream> 
+using namespace std;
 
-void main(void)
+class vector
 {
-    cout << "Hello World";
+    public:
+        float x;
+        float y;
+
+    vector(float x_val, float y_val)
+    {
+        x = x_val;
+        y = y_val;
+    }
+};
+
+int main()
+{
+    vector vec1(100,100);
+
+    string input;
+
+    int ff[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    for (auto &&i : ff)
+    {
+        cout << i <<endl;
+    }
+    
+    //cout << *ff; 
+
+    cout << "x:" << vec1.x << "y:" << vec1.y << endl;
+
+    cin >> input;
+    
+    int f = input.find(' ');
+
+    cout << "in:" << input << "find:" << f << endl;
+
+    return 0;
 }
