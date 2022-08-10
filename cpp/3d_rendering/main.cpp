5f7eb074fbc328b144cd25c35556f61daf9a7873
slin <n-jaeggi@gmx.ch>
2022-08-10 17:41:27 +0100

    draw custom shape with vertexes

--- cpp/3d_rendering/main.cpp
+++ cpp/3d_rendering/main.cpp
@@ -1,23 +1,38 @@
 #include <SFML/Graphics.hpp>
 
+// i want to #1:
+// place quads on screen (code)
+// set player pos and direcrion (+viewport in settings)
+// -> create 3d-rendering, what the player would see in a 3d room
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
 
-    while (window.isOpen())
+    sf::RenderWindow main2dWindow(sf::VideoMode(800, 600), "3D-Rendering Debug", sf::Style::Close, settings);
+
+    std::vector<sf::Vertex> quad;
+    float x_coords[4] = {10.0f, 100.0f, 200.0f, 10.0f};
+    float y_coords[4] = {10.0f, 10.0f, 100.0f, 100.0f};
+
+    for(uint8_t i = 0; i < 4; i++){
+        quad.push_back(sf::Vertex(sf::Vector2f(x_coords[i], y_coords[i]), sf::Color::Blue));
+    }
+
+
+    while (main2dWindow.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (main2dWindow.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
+                main2dWindow.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        main2dWindow.clear();
+        main2dWindow.draw(&quad[0], quad.size(), sf::Quads);
+        main2dWindow.display();
     }
 
     return 0;
